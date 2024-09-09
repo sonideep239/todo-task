8a9ebd79b56ed434629d35b74e541befaca0e8c8
Deep Soni <deep.soni@bcreative.in>
2024-09-09 21:45:56 +0530

    task submit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [data, setData] = useState([]);
+  const [SelectedItem, setSelectedItem] = useState([]);
+
+  const handleClick = () => {
+    if (inputValue) {
+      let arr = [...data];
+      arr.push({ inputValue });
+      setData(arr);
+      setInputValue('');
+    }
+  };
+
+  const handleItemClick = (index) => {
+    const arr = [...SelectedItem];
+    const itemIndex = arr.indexOf(index);
+    if (itemIndex !== -1) {
+      arr.splice(itemIndex, 1);
+    } else {
+      arr.push(index);
+    }
+    setSelectedItem(arr);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter a value"
+        />
+        <button onClick={handleClick}>Add</button>
+        <div>
+          {data?.map((item, index) => {
+            return (
+              <div
+                onClick={() => handleItemClick(index)}
+                style={{ backgroundColor: "#ffffff", width: 50, cursor: 'pointer' }}>
+                <p
+                  style={{
+                    color: '#000000',
+                    textDecoration: SelectedItem.includes(index) ? 'underline' : 'none',
+                  }}>{item.inputValue}</p>
+              </div>
+            )
+          })}
+        </div>
       </header>
     </div>
   );
